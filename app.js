0eca8458532f4bef63b25ff10d71a29a4c9b3a65
Zac Wheeler <wheelerzac@yahoo.com>
2023-05-25 18:31:46 -0500

    added to app.js and shapes.js

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-const inquirer = require('inquirer');
-const {Circle} = require('./lib/shapes')
+const inquirer = require("inquirer");
+const { Circle, Square, Triangle } = require("./lib/shapes");
+const fs = require("fs");
 const questions = [
-    {
+  {
     type: "input",
-    message: "What color do you want your shape to be?",
+    message: "What color do you want your logo to be?",
     name: "color",
-    
-    
+  },
+  {
+    type: "input",
+    message: "What shape would you like your logo?",
+    name: "shape",
+  },
+  {
+    type: "input",
+    message: "What characters would you like displayed?",
+    name: "characters",
+  },
+];
+
+inquirer
+  .prompt(questions)
+  // .then(function (data))
+  .then(({ color, shape, characters }) => {
+    let shape;
+    if (shape === "circle") {
+      shape = new Circle(color, characters);
+    } else if (shape === "triangle") {
+      shape = new Triangle(color, characters);
+    } else {
+      shape = new Square(color, characters);
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ]
 
-    inquirer
-    .prompt(questions)
-    // .then(function (data))
-    .then((data) => {
- console.log(data);
- let shape; 
- shape = new Circle()
- shape.getColor(data.color)
- console.log(shape);
-    })
+    return shape.render();
+  })
+  .then((templateString) => {
+    fs.writeFile("log.svg", templateString, function (err) {
+      err ? console.log(err) : console.log('Success!');
+    });
+  });
 
-//     fs.writeFile('log.txt', 'replace this with whatever we are wanting in file', (err) =>
-//   err ? console.error(err) : console.log('Success!')
-// );
+
