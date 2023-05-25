69c3acab8f098c3c82786bc9f0f8ef4a2d544e07
Zac Wheeler <wheelerzac@yahoo.com>
2023-05-24 21:07:45 -0500

    added to app.js and linked inquirer

---
+++ .vscode/question.js
@@ -0,0 +1,19 @@
+const questions = [
+{
+type: "input",
+message: "what type of text image do you want?",
+name: "file type",
+
+
+}
+
+
+
+
+
+
+
+
+
+
+]
--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-console.log('test');
-console.log('i made a change');
+const inquirer = require('inquirer');
+const {Circle} = require('./lib/shapes')
+const questions = [
+    {
+    type: "input",
+    message: "What color do you want your shape to be?",
+    name: "color",
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ]
+
+    inquirer
+    .prompt(questions)
+    // .then(function (data))
+    .then((data) => {
+ console.log(data);
+ let shape; 
+ shape = new Circle()
+ shape.getColor(data.color)
+ console.log(shape);
+    })
+
+//     fs.writeFile('log.txt', 'replace this with whatever we are wanting in file', (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
