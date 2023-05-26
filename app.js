436b3eb1479c408518ebbb96c2f8641339731a20
Zac Wheeler <wheelerzac@yahoo.com>
2023-05-25 23:10:30 -0500

    added more to app.js

--- app.js
+++ app.js
@@ -9,27 +9,32 @@ const questions = [
   },
   {
     type: "input",
+    message: "What color do you want your logo text to be?",
+    name: "textcolor",
+  },
+  {
+    type: "input",
     message: "What shape would you like your logo?",
     name: "shape",
   },
   {
     type: "input",
     message: "What characters would you like displayed?",
-    name: "characters",
+    name: "text",
   },
 ];
 
 inquirer
   .prompt(questions)
   // .then(function (data))
-  .then(({ color, shape, characters }) => {
-    let shape;
+  .then(({ color, textcolor, shape, text }) => {
+    // let shape;
     if (shape === "circle") {
-      shape = new Circle(color, characters);
+      shape = new Circle(color, textcolor, text);
     } else if (shape === "triangle") {
-      shape = new Triangle(color, characters);
+      shape = new Triangle(color, textcolor, text);
     } else {
-      shape = new Square(color, characters);
+      shape = new Square(color, textcolor, text);
     }
 
     return shape.render();
